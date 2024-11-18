3c9b28a0ff3a8419609d4e89a1cbdafb1dc571e3
ErmiasKetos <123589824+ErmiasKetos@users.noreply.github.com>
2024-11-18 15:36:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from src.registration_calibration import registration_calibration_page
 from src.inventory_review import inventory_review_page
+st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
 
 # Set page configuration
 st.set_page_config(page_title="KETOS WB PMS", layout="wide")
