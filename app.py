e9369b2ef6faf1402ab24eaab3af0e997bd64826
ErmiasKetos <123589824+ErmiasKetos@users.noreply.github.com>
2024-11-18 17:29:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,18 @@ from src.inventory_review import inventory_review_page
 st.set_page_config(page_title="Probe Management System", layout="wide")
 
 # Sidebar Navigation
-st.sidebar.title("KETOS WB PMS")
+st.sidebar.title("CalMS")
 page = st.sidebar.radio(
     "Navigate",
     ["Probe Registration & Calibration", "Inventory Review"],
 )
+
+# Shared Save Location Setting
+if "save_location" not in st.session_state:
+    st.session_state["save_location"] = st.radio(
+        "Select where to save the inventory file:",
+        ["Local Computer", "Google Drive"],
+    )
 
 # App Navigation
 if page == "Probe Registration & Calibration":
