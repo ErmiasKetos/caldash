1bd573a5625caf9a0bbf9ddbb124694ff48ea216
ErmiasKetos <123589824+ErmiasKetos@users.noreply.github.com>
2024-11-19 02:42:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 SCOPES = [
     'openid',
     'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile'
 ]
