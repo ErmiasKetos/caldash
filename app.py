1dc72382e7a5479740ac132c61b843ac986c7d41
ErmiasKetos <123589824+ErmiasKetos@users.noreply.github.com>
2024-11-20 00:52:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import logging
 import os
 from datetime import datetime
 from src.drive_manager import DriveManager
-from src.registration_calibration import registration_calibration_page
 from src.inventory_review import inventory_review_page
 from src.inventory_manager import initialize_inventory
+from src.registration_page import registration_page
+from src.calibration_page import calibration_page
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
